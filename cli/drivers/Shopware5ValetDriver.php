4a09e795fe7f23665cc12f0a1ca705b214acbe49
Nicky Gerritsen <nicky@letstalk.nl>
2022-10-13 16:37:54 +0200

    Add Shopware 5 driver

---
+++ cli/drivers/Shopware5ValetDriver.php
@@ -0,0 +1,52 @@
+<?php
+
+class Shopware5ValetDriver extends ValetDriver
+{
+    /**
+     * Determine if the driver serves the request.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return bool
+     */
+    public function serves($sitePath, $siteName, $uri)
+    {
+        return file_exists($sitePath.'/shopware.php');
+    }
+
+    /**
+     * Determine if the incoming request is for a static file.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string|false
+     */
+    public function isStaticFile($sitePath, $siteName, $uri)
+    {
+        if ($this->isActualFile($staticFilePath = $sitePath.'/'.$uri)) {
+            return $staticFilePath;
+        }
+
+        return false;
+    }
+
+    /**
+     * Get the fully resolved path to the application's front controller.
+     *
+     * @param  string  $sitePath
+     * @param  string  $siteName
+     * @param  string  $uri
+     * @return string
+     */
+    public function frontControllerPath($sitePath, $siteName, $uri)
+    {
+        $_SERVER['DOCUMENT_ROOT'] = $sitePath;
+        $_SERVER['SCRIPT_FILENAME'] = $sitePath.'/shopware.php';
+        $_SERVER['SCRIPT_NAME'] = '/shopware.php';
+        $_SERVER['PHP_SELF'] = '/shopware.php';
+
+        return $sitePath.'/shopware.php';
+    }
+}
